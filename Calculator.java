247862d6df43bf365446a6d29678d5a6af144a94
Sameer-Ahamed-T <sameerahamed1609@gmail.com>
2025-08-19 20:47:26 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -7,7 +7,7 @@ class Calculator
                 System.out.println("You can do operation in the calculator \nIf you want to exit the program enter the q Button!!!"); 
                 int i=1;
                 int temp=0; 
-							  char c='+';int n;
+				char c='+';int n;
                  while(true) 
                  { 
                    if(i==1)
@@ -15,10 +15,10 @@ class Calculator
                            n=sc.nextInt(); 
                            c=sc.next().charAt(0); 
                            temp=sc.nextInt();
-										 			 i++;
+						   i++;
                    } 
                    else
-										        n=sc.nextInt();
+							n=sc.nextInt();
                    int t=temp;
                    switch(c)
                    { 
@@ -42,34 +42,34 @@ class Calculator
                    } 
                    char ch;
                    ch=sc.next().charAt(0);
-									 if(ch=='=')
-									 {
-										 			System.out.println(""+  n + c + t +"="+temp);
-										 			char e;
-										 			e=sc.next().charAt(0);
-										 			if(e=='q')
-											 						 break;
-										 			else if(e=='+'||e=='-'||e=='*'||e=='/'||e=='%')
-													{
-																	c=e;	
-											 						continue;
-													}
-										 			else
-										 			{
-											 					  System.out.println("Sorry!!!You Entered a unexpected input.");
-											 						break;
-										 			}
-									 }
-									 else if(ch=='+'||ch=='-'||ch=='*'||ch=='/'||ch=='%')
-									 {
-										 							c=ch;
-										 							continue;
-									 }
-									 else
-									 {
-										 							System.out.println("Sorry!!!You Entered a unexpected input.");
-										 							break;
-									 }
+					if(ch=='=')
+					{
+							System.out.println(""+  n + c + t +"="+temp);
+							char e;
+							e=sc.next().charAt(0);
+							if(e=='q')
+								break;
+							else if(e=='+'||e=='-'||e=='*'||e=='/'||e=='%')
+							{
+								c=e;	
+								continue;
+							}
+							else
+							{
+								System.out.println("Sorry!!!You Entered a unexpected input.");
+								break;
+							}
+					}
+					else if(ch=='+'||ch=='-'||ch=='*'||ch=='/'||ch=='%')
+					{
+							c=ch;
+							continue;
+					}
+					else
+					{
+							System.out.println("Sorry!!!You Entered a unexpected input.");
+							break;
+					}
                  }
         } 
 }
